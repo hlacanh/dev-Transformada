6746d4cae327eb7a3d926ed32843789a386e0a9d
Team Core Bancario <gdt-core@domcoin.net>
2025-04-08 14:20:02 -0600

    add json dummy

--- src/main/java/org/example/route/KafkaToLogRoute.java
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -13,5 +13,50 @@ public class KafkaToLogRoute extends RouteBuilder {
             .log("Mensaje original desde Kafka: ${body}")
             .to("jslt:classpath:transformacion.jslt")
             .log("Mensaje transformado: ${body}");
+
+        from("timer:hardcoded?repeatCount=1")
+            .routeId("hardcoded-jslt-log")
+            .setBody(constant("{" +
+                    " \"body\":{" +
+                    " \"customerIds\":[" +
+                    "  {" +
+                    "      \"customerId\":\"100103\", "+
+                    "      \"customerRole\":\"benefitial owner\" "+
+                    "  }"+
+                    " ], "+
+                    "\"properties\":[ "+
+                    "    {"+
+                    "        \"ACCOUNT\":{"+
+                    "              \\"L.TI.GARAN\":\"20\","+
+                    "              \"L.DET.GARANTIA\":\"40\","+
+                    "              \"CASO.CREATIO\":\"1\","+
+                    "              \"N.RESOLUCION\":\"1\","+
+                    "              \"Fecha.Form\":\"20250301\","+
+                    "              \"Tipo.Docto.Form\":\"6\""+
+                    "      }"+
+                    "    }"+
+                          
+                    "],"+
+                    "\"productId\":\"NOMINAS.CON.CONVENIO\","+
+                    "\"masterArrangementId\":\"\","+
+                    "\"roleName\":\"\","+
+                    "\"transactionAmount\":\"0\","+
+                    "\"reason\":\"\",
+                    "\"simulationRunReference\":\"\","+
+                    "\"branch\":\"\","+
+                    "\"lineOfBusiness\":\"\","+
+                    "\"UUID\":\"123\",
+                    "\"Terminal\":\"TERMINAL1\","+
+                    "\"Channel\":\"1\","+
+                    "\"User\":\"HLACAN1\","+
+                    "\"arrangementId\":\"NEW\","+
+                    "\"currencyId\":\"GTQ\","+
+                    "\"activityId\":\"LENDING-NEW-ARRANGEMENT\""+
+                  "} }"
+                
+            ))
+            ("Mensaje hardCoded ${body}")
+            .to("jslt:classpath:transformacion.jslt")
+            .log("Mensaje hardcoded: ${body}");
     }
 }
