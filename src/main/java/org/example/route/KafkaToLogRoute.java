638fa8040b9fca5d2c739a373609ca1ab4f1a164
Team Core Bancario <gdt-core@domcoin.net>
2025-04-08 14:37:18 -0600

    add json dummy

--- src/main/java/org/example/route/KafkaToLogRoute.java
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -8,11 +8,41 @@ public class KafkaToLogRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("kafka:my-topic10?brokers=cluster-nonprod01-kafka-bootstrap.amq-streams-kafka:9092")
-            .routeId("kafka-jslt-log")
-            .log("Mensaje original desde Kafka: ${body}")
-            .to("jslt:classpath:transformacion.jslt")
-            .log("Mensaje transformado: ${body}");
 
+
+        from("timer:hardcoded?repeatCount=1")
+            .routeId("hardcoded-jslt-log")
+            .setBody(constant("{" +
+                    " \"body\":{" +
+                    " \"customerIds\":[" +
+                    "  {" +
+                    "      \"customerId\":\"100103\", "+
+                    "      \"customerRole\":\"benefitial owner\" "+
+                    "  }"+
+                    " ], "+
+                    "\"properties\":[ "+
+                    "    {"+
+                    "        \"ACCOUNT\":{"+
+                    "              \"L.TI.GARAN\":\"20\","+
+                    "              \"L.DET.GARANTIA\":\"40\","+
+                    "              \"CASO.CREATIO\":\"1\","+
+                    "              \"N.RESOLUCION\":\"1\","+
+                    "              \"Fecha.Form\":\"20250301\","+
+                    "              \"Tipo.Docto.Form\":\"6\""+
+                    "      }"+
+                    "    }"+
+                          
+                    "],"+
+                    "\"productId\":\"NOMINAS.CON.CONVENIO\","+
+                    "\"masterArrangementId\":\"\","+
+                    "\"roleName\":\"\","+
+                    "\"UUID\":\"123\",
+                    "\"activityId\":\"LENDING-NEW-ARRANGEMENT\""+
+                  "} }"
+                
+            ))
+            .log("Mensaje hardCoded ${body}")
+            .to("jslt:classpath:transformacion.jslt")
+            .log("Mensaje hardcoded: ${body}");
     }
 }
