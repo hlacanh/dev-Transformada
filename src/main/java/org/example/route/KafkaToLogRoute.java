6ce85685910873c69613fc5881ea77e76dd46dcd
Team Core Bancario <gdt-core@domcoin.net>
2025-04-08 14:52:31 -0600

    add json dummy

--- src/main/java/org/example/route/KafkaToLogRoute.java
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -10,35 +10,6 @@ public class KafkaToLogRoute extends RouteBuilder {
     public void configure() {
         from("kafka:my-topic10?brokers=cluster-nonprod01-kafka-bootstrap.amq-streams-kafka:9092")
             .routeId("kafka-jslt-log")
-            .setBody(constant("{" +
-                    " \"body\":{" +
-                    " \"customerIds\":[" +
-                    "  {" +
-                    "      \"customerId\":\"100103\", "+
-                    "      \"customerRole\":\"benefitial owner\" "+
-                    "  }"+
-                    " ], "+
-                    "\"properties\":[ "+
-                    "    {"+
-                    "        \"ACCOUNT\":{"+
-                    "              \"L.TI.GARAN\":\"20\","+
-                    "              \"L.DET.GARANTIA\":\"40\","+
-                    "              \"CASO.CREATIO\":\"1\","+
-                    "              \"N.RESOLUCION\":\"1\","+
-                    "              \"Fecha.Form\":\"20250301\","+
-                    "              \"Tipo.Docto.Form\":\"6\""+
-                    "      }"+
-                    "    }"+
-                          
-                    "],"+
-                    "\"productId\":\"NOMINAS.CON.CONVENIO\","+
-                    "\"masterArrangementId\":\"\","+
-                    "\"roleName\":\"\","+
-                    "\"UUID\":\"123\",
-                    "\"activityId\":\"LENDING-NEW-ARRANGEMENT\""+
-                  "} }"
-                
-            ))
             .log("Mensaje original desde Kafka: ${body}")
             .to("jslt:classpath:transformacion.jslt")
             .log("Mensaje transformado: ${body}");
