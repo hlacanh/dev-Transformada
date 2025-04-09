1e1f40b527516f81d368658fe07eb19f0b542be2
Team Core Bancario <gdt-core@domcoin.net>
2025-04-08 18:15:09 -0600

    add json dummy

--- src/main/java/org/example/route/KafkaToLogRoute.java
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -6,16 +6,22 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class KafkaToLogRoute extends RouteBuilder {
 
+    JsonDataFormat jsonDataFormat = new JsonDataFormat();
+    jsonDataFormat.setPrettyPrint(false);
+
     @Override
     public void configure() {
         from("kafka:my-topic10?brokers=cluster-nonprod01-kafka-bootstrap.amq-streams-kafka:9092")
+            .routeId("kafka-jslt-log")
             .process(exchange -> {
                 String rawBody = exchange.getMessage().getBody(String.class);
                 System.out.println("Mensaje original desde kafka (procesador):" + rawBody);
         
             })
-            .routeId("kafka-jslt-log")
-            .log("Mensaje original desde Kafka: ${body}")
+            .log(LoggingLevel.INFO, "Mensaje original desde kafka (log): $ {body}")
+            .marshal(jsonDataFormat)
+            .log("Mensaje original sin identacion: ${body}")
+            .unmarshal(jsonDataFormat)
             .to("jslt:classpath:transformacion.jslt")
             .log("Mensaje transformado: ${body}");
     }
