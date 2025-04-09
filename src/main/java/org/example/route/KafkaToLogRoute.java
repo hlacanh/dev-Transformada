5b7ccfd628d971e0181e5686165d7ba08f183dc6
Team Core Bancario <gdt-core@domcoin.net>
2025-04-08 18:06:10 -0600

    add json dummy

--- src/main/java/org/example/route/KafkaToLogRoute.java
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -9,9 +9,14 @@ public class KafkaToLogRoute extends RouteBuilder {
     @Override
     public void configure() {
         from("kafka:my-topic10?brokers=cluster-nonprod01-kafka-bootstrap.amq-streams-kafka:9092")
+            .process(exchange -> {
+                String rawBody = exchange.getMessage().getBody(String.class);
+                System.out.println("Mensaje original desde kafka (procesador):" + rawBody);
+        
+            })
             .routeId("kafka-jslt-log")
-            .log("Mensaje original desde Kafka: ${simple(${body})}")
+            .log("Mensaje original desde Kafka: ${body}")
             .to("jslt:classpath:transformacion.jslt")
-            .log("Mensaje transformado: ${simple(${body})}");
+            .log("Mensaje transformado: ${body}");
     }
 }
