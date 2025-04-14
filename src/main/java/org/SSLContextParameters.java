758b3e3578eea6bf7df9950d15578fa7c0efb863
Team Core Bancario <gdt-core@domcoin.net>
2025-04-14 11:35:29 -0600

    add consume uri

--- src/main/java/org/SSLContextParameters.java
+++ src/main/java/org/SSLContextParameters.java
@@ -4,18 +4,34 @@ import javax.net.ssl.X509TrustManager;
 import java.security.cert.X509Certificate;
 import org.apache.camel.support.jsse.SSLContextParameters;
 import org.apache.camel.support.jsse.TrustManagersParameters;
+import org.springframework.context.annotation.Bean;
 
-@Bean("sslContextParameters")
-public SSLContextParameters createSslContextParameters() {
-    SSLContextParameters sslContextParameters = new SSLContextParameters();
+public class SSLContextParametersConfig {
     
-    TrustManagersParameters trustManagersParameters = new TrustManagersParameters();
-    trustManagersParameters.setTrustManager(new X509TrustManager() {
-        public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-        public void checkServerTrusted(X509Certificate[] chain, String authType) {}
-        public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-    });
-    sslContextParameters.setTrustManagers(trustManagersParameters);
-    
-    return sslContextParameters;
+    @Bean("sslContextParameters")
+    public SSLContextParameters createSslContextParameters() {
+        SSLContextParameters sslContextParameters = new SSLContextParameters();
+        
+        TrustManagersParameters trustManagersParameters = new TrustManagersParameters();
+        trustManagersParameters.setTrustManager(new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                // No verificación
+            }
+            
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                // No verificación
+            }
+            
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        });
+        
+        sslContextParameters.setTrustManagers(trustManagersParameters);
+        
+        return sslContextParameters;
+    }
 }
